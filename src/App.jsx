17be7b4c05306805cf1780f07ba0a3ae6eb88201
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-06 00:36:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Blogs />
+    </>
+  )
+}
+
+export default App
